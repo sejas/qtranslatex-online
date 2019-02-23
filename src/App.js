ad2167f1322ac718ab66cb80d4dd23f85ff4a060
Sejas <antonio@sejas.es>
2019-02-23 18:09:58 +0000

    basic html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>QTranslate X Extractor Online</h1>
+        </header>
+        <main>
+          {/* Input */}
+          <section>
+            <article class="Viewer">
+              <h2 className="Viewer-title">
+                Paste your QTranslate Content here:
+              </h2>
+              <textarea className="Viewer-input" name="source" />
+            </article>
+          </section>
+
+          {/* Results */}
+          <section>
+            <article class="Viewer">
+              <h2 className="Viewer-title"> Your result:</h2>
+              <textarea className="Viewer-input" name="result" />
+            </article>
+          </section>
+        </main>
+        <footer>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://sejas.es"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Developed with ♥ by Antonio Sejas
           </a>
-        </header>
+        </footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
