a15023b6c04150136d363e7ffd823ab120135fd1
Sejas <antonio@sejas.es>
2019-02-23 19:30:23 +0000

    use gh-pages to publish the content

---
+++ build/precache-manifest.c26ea28ef2e06638942e28fbd6a943a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1761f0a6d01b39812d61",
+    "url": "/qtranslatex-online/static/js/runtime~main.1761f0a6.js"
+  },
+  {
+    "revision": "5747ae48cb4c7f38b1d1",
+    "url": "/qtranslatex-online/static/js/main.5747ae48.chunk.js"
+  },
+  {
+    "revision": "e575b208b7607a47204f",
+    "url": "/qtranslatex-online/static/js/2.e575b208.chunk.js"
+  },
+  {
+    "revision": "5747ae48cb4c7f38b1d1",
+    "url": "/qtranslatex-online/static/css/main.9693cb11.chunk.css"
+  },
+  {
+    "revision": "53dca0011c0c9ad57bc2ce1af50f4c6f",
+    "url": "/qtranslatex-online/index.html"
+  }
+];
